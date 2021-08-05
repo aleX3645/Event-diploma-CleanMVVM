3d37608ec914e24c6742e907456e4320c6b5c0e8
Алексей <alexche3645@mail.ru>
2021-08-05 07:49:07 +0300

    add base navigation and list module

---
+++ app/src/main/java/com/alex3645/app/presentation/NavHostActivity.kt
@@ -0,0 +1,29 @@
+package com.alex3645.app.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.alex3645.eventdiploma_mvvm.R
+import com.alex3645.eventdiploma_mvvm.databinding.ActivityNavHostBinding
+
+class NavHostActivity : AppCompatActivity() {
+
+    private val navController get() = findNavController(R.id.navHostFragment)
+    private lateinit var binding: ActivityNavHostBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityNavHostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        initBottomNavigation()
+    }
+
+    private fun initBottomNavigation() {
+        binding.bottomNavigation.setupWithNavController(navController)
+
+        // Disables reselection of bottom menu item, so fragments are not recreated when clicking on the same menu item
+        //binding.bottomNavigation.setOnNavigationItemReselectedListener { }
+    }
+}
---
+++ feature_event_list/src/main/java/com/alex3645/feature_event_list/presentation/eventRecyclerView/RecyclerFragment.kt
@@ -0,0 +1,36 @@
+package com.alex3645.feature_event_list.presentation.eventRecyclerView
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.alex3645.feature_event_list.databinding.FragmentRecyclerListBinding
+
+class RecyclerFragment: Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    private var _binding: FragmentRecyclerListBinding? = null
+    // This property is only valid between onCreateView and
+// onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentRecyclerListBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
